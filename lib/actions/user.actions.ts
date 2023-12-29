02e3f3868e31c38c6740c534ba828f99bdd9ee6b
sinkoosha <sinkoosha@gmail.com>
2023-12-30 00:52:12 +0330

    c5

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,14 +3,15 @@
 import { CreateUserParams } from "@/types";
 import { connectToDatabase } from "../database";
 import { handleError } from "../utils";
+import User from "../database/models/user.model";
 
-export const createUser = async (user: CreateUserParams) => {
+export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase();
-    const newUser = await User.create(user);
 
-    return JSON.parse(newUser);
+    const newUser = await User.create(user);
+    return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     handleError(error);
   }
-};
+}
